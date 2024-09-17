2781b6ce781c1213f142c3f1178c65dbe151ab70
S Sarath Kumar <153589031+S-a-r-a-t-h@users.noreply.github.com>
2024-09-17 18:00:36 +0530

    creditcard.py

--- creditcard.py
+++ creditcard.py
@@ -3,7 +3,6 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
-from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report, confusion_matrix
 from imblearn.over_sampling import SMOTE
 import matplotlib.pyplot as plt
